653b304970f86fb8341327915177e939fc716cc9
寇有全 <kou_youquan@163.com>
2018-04-18 15:45:26 +0800

    1、添加新项目

---
+++ FFAVPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FFAVPlayer
+//
+//  Created by cts on 2018/4/18.
+//  Copyright © 2018年 cts. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FFAVPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FFAVPlayer
+//
+//  Created by cts on 2018/4/18.
+//  Copyright © 2018年 cts. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
